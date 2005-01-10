466899a691a5fd0fcb19f257a43f95cccf22da85
erik <erik>
2005-01-10 14:28:14 +0000

    no more lexer shit

--- src/main.c
+++ src/main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern FILE *yyin;
-void yyparse(void);
-
 int main(int argc, char **argv)
 {
-	yyin = stdin;
-//	yyparse();
-	yylex();
 	return EXIT_SUCCESS;
 }
